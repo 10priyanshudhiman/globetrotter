63fa78ac985a07cde2ee325abb70d0ef12352d89
Priyanshu <10priyanshudhiman@gmail.com>
2025-03-04 08:23:43 +0530

    errors

--- frontend/globetrotter-ui/src/services/fetchUserDetails.js
+++ frontend/globetrotter-ui/src/services/fetchUserDetails.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import {useEffect} from "react";
 import { getRequestToAPI } from "../services/api";
 import URLS from "../urls";
 import store from "../store/store";
