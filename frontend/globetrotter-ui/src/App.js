f75496ad5cb2c83e41e1afb8c84885b1754547d8
Priyanshu <10priyanshudhiman@gmail.com>
2025-03-06 01:11:47 +0530

    test

--- frontend/globetrotter-ui/src/App.js
+++ frontend/globetrotter-ui/src/App.js
@@ -1,37 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Dashboard from "./views/Dashboard";
-import Login from "./views/Login";
-import ResponseSelection from "./views/ResponseSelection";
-import Score from "./views/Score";
-import Header from "./views/Header";
-import "./assets/css/app.css";
-import FetchUserDetails from "./services/fetchUserDetails";
+import React, { Suspense, lazy } from "react";
+const Dashboard = lazy(() => import("./views/Dashboard"));
+const Login = lazy(() => import("./views/Login"));
+const ResponseSelection = lazy(() => import("./views/ResponseSelection"));
+const Score = lazy(() => import("./views/Score"));
+const Header = lazy(() => import("./views/Header"));
 
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
-}
-
-function AppContent() {
   const location = useLocation();
   const isLoggedIn = localStorage.getItem("userId");
 
   return (
     <>
-      <FetchUserDetails/>
-      {isLoggedIn && location.pathname !== "/" && <Header />}
-      <Routes>
-        <Route index path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/responses/:cardId" element={<ResponseSelection />} />
-        <Route path="/score" element={<Score />} />
-      </Routes>
+      <FetchUserDetails />
+      {isLoggedIn && location.pathname !== "/" && (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+        </Suspense>
+      )}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/responses/:cardId" element={<ResponseSelection />} />
+          <Route path="/score" element={<Score />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
-
-export default App;
+export default App
